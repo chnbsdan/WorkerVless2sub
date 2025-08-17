1af348efc3c071046960ceb01b05e0dc10a2d8a9
chnbsdan <chnbsdan@gmail.com>
2025-08-17 10:51:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -42,10 +42,10 @@ let MamaJustKilledAMan = ['telegram', 'twitter', 'miaoko'];
 let proxyIPPool = [];
 let socks5Data;
 let alpn = 'h3';
-let 网络备案 = `<a href='https://t.me/CMLiussss'>萌ICP备-20240707号</a>`;//写你自己的维护者广告
+let 网络备案 = `<a href='https://hangdn.com'>BSDAN 智能家居制造商</a>`;//写你自己的维护者广告
 let 额外ID = '0';
 let 加密方式 = 'auto';
-let 网站图标, 网站头像, 网站背景, xhttp = '';
+let 网站图标, 网站头像, 网站背景, xhttp = 'https://tc.1356666.xyz/file/1751375155852_2039852.jpg';
 async function 整理优选列表(api) {
 	if (!api || api.length === 0) return [];
 
@@ -1469,4 +1469,5 @@ async function subHtml(request) {
 			"content-type": "text/html;charset=UTF-8",
 		},
 	});
+
 }
